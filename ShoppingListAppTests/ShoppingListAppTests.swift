0b0e125bc4d5b8aa3f52c31abf32f85131a6e3af
CDo101463447 <christian.do@georgebrown.ca>
2025-02-21 22:43:01 -0500

    first commit

---
+++ ShoppingListAppTests/ShoppingListAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingListAppTests.swift
+//  ShoppingListAppTests
+//
+//  Created by Christian Do on 2/21/25.
+//
+
+import Testing
+@testable import ShoppingListApp
+
+struct ShoppingListAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
